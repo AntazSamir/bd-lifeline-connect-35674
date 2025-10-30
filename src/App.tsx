b16dff7d28b6d38429ce145eccecde0206ea4798
Antaz Samiul <rocksamir098@gmail.com>
2025-10-31 00:23:06 +0600

    Add About route to the application

--- src/App.tsx
+++ src/App.tsx
@@ -51,6 +51,7 @@ const App = () => (
             <Route path="/admin" element={<AdminPanel />} />
             <Route path="/confirm-email" element={<EmailConfirmation />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
             <Route path="/reset-password" element={<ResetPassword />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
